abb5b76a520598f697965e0fbc77356d1097079c
yjtsaie <jeffrey_lii_2002@hotmail.com>
2016-03-26 12:00:09 +0800

    Update plot2.R
    add comments

--- plot2.R
+++ plot2.R
@@ -1,8 +1,5 @@
 library(lubridate)
-
-
-
-
+## read in data and get the subset of working data set
 con <- file("~./R/household_power_consumption.txt","r") 
 full_data <- read.table(con, sep = ";" , header = T , na.strings ="", stringsAsFactors= F) 
 close(con) 
@@ -12,12 +9,13 @@ data1 <- subset(full_data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"
 rm(full_data)
 datetime <- paste(data1$Date, data1$Time)
 data1$Datetime <- as.POSIXct(datetime) 
-#combine date and time 
  
-
+# clean up data and plotiton screen
 data1$Global_active_power[data1$Global_active_power=="?"] <- NA 
 data1$Global_active_power <- as.numeric(data1$Global_active_power) 
+plot(data1$Global_active_power ~ data1$Datetime, type = "l",ylab = "Global Active Power (kilowatts)", xlab = "")
 
+## plot into png file plot2.png
 png(file="plot2.png",width = 480, height = 480)
 plot(data1$Global_active_power ~ data1$Datetime, type = "l",ylab = "Global Active Power (kilowatts)", xlab = "")
 dev.off()
