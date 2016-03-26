b01d5a79dd41d905c699d2785f2fd3ae3aec91ce
yjtsaie <jeffrey_lii_2002@hotmail.com>
2016-03-26 15:00:56 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -18,16 +18,16 @@ data1$Sub_metering_3[data1$Sub_metering_3=="?"] <- NA
 
 ## plot on screen 
 plot(data1$Datetime,data1$Sub_metering_1, type = "l",ylab = "Engery Sub metering", xlab = "")
-lines(data1$Sub_metering_2 ~ data1$Datetime, col="Red")
-lines(data1$Sub_metering_3 ~ data1$Datetime, col="Blue")
+lines(data1$Datetime,data1$Sub_metering_2, col="Red")
+lines(data1$Datetime, data1$Sub_metering_3, col="Blue")
 legend("topright", legend = c("sub_metering_1", "sub_metering_2","sub_metering_3"),
-        lty = 1, lwd=2, col=c("Black","Red","Blue"), xjust = 1, yjust = 1)
+       lty = 1, lwd=2, col=c("Black","Red","Blue"), xjust = 1, yjust = 1)
        
 ## create plot3.png file 
 png(file="plot3.png",width = 480, height = 480)
 plot(data1$Datetime,data1$Sub_metering_1, type = "l",ylab = "Engery Sub metering", xlab = "")
-lines(data1$Sub_metering_2 ~ data1$Datetime, col="Red")
-lines(data1$Sub_metering_3 ~ data1$Datetime, col="Blue")
+lines(data1$Datetime,data1$Sub_metering_2, col="Red")
+lines(data1$Datetime, data1$Sub_metering_3, col="Blue")
 legend("topright", legend = c("sub_metering_1", "sub_metering_2","sub_metering_3"),
        lty = 1, lwd=2, col=c("Black","Red","Blue"), xjust = 1, yjust = 1)
 dev.off()
